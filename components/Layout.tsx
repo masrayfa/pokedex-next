548bf002e262dcefe5c4e95e3448129175db85bd
masrayfa <maselon2030@gmail.com>
2022-11-04 22:40:58 +0700

    fix: consistency bg-color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Layout: React.FC<Props> = (props) => {
   const { children, className } = props
   return (
-    <div className="bg-[#19202A] h-screen lg:h-max">
+    <div className="bg-[#19202A] ">
       <Header title="Pokedex" />
       <main className={`${className}`}>{children}</main>
       <footer></footer>
